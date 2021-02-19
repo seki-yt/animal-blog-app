50195e5bd378e309e6eefb968c2843e6f35057f2
seki_yuts <>
2021-02-19 17:31:30 +0900

    create機能を追加したよ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,28 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+    #=> Article.newでデータを入れる空の箱を作ってます
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      # => これで保存するよ
+      redirect_to article_path(@article), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+    #=> requireでarticleがないとだめと要求
+    #=> permitでarticleのtitleとcontentだけを許す
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-
-  
+  validates :title, presence: true
+  #=> presence は入力されてるかcheck
+  validates :content, presence: true
 end
