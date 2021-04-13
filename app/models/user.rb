862cf85b81887d7972eeb92e46b050f02fd22617
seki_yuts <>
2021-04-13 15:54:34 +0900

    follow機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,12 @@ class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   #=> rails Activerecordでつなぐために書いている
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :age, :birthday, :gender, to: :profile, allow_nil: true
   #=> delegateは簡単にbirthdayなどのメゾットを作れる
   #=> allow_nil: true でぼっち演算子を勝手に付けてくれる
@@ -46,6 +52,20 @@ class User < ApplicationRecord
     profile&.nickname || self.email.split('@').first
   end
 
+  def follow!(user)
+    user_id = get_user_id(user)
+    following_relationships.create!(following_id: user_id)
+  end
+
+  def unfollow!(user)
+    user_id = get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
+    relation.destroy!
+  end
+
+  def has_followed?(user)
+    following_relationships.exists?(following_id: user.id)
+  end
   def prepare_profile
     profile || build_profile
   end
@@ -55,6 +75,15 @@ class User < ApplicationRecord
       profile.avater
     else
       'default-avatar.png'
+    end
+  end
+
+  private
+  def get_user_id(user)
+    if user.is_a?(User)
+      user_id = user.id
+    else
+      user_id = user
     end
   end
 end
